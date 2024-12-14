aa2d2097eaf9d5bad368da79a7deceb5f40a3bb9
KIRAN KUMAR S <136549548+kiran-kumars953@users.noreply.github.com>
2024-12-14 14:31:23 +0530

    Program3.py

--- Program3.py
+++ Program3.py
@@ -1,5 +1,9 @@
-a = int(input("Enter a number: "))
-if a % 2 == 0:
-    a -= 1
-result = [2 * i - 1 for i in range(1, a + 1, 2)]
-print(", ".join(map(str, result)))
+count = int(input("Enter count number: "))
+if count % 2 != 1:
+    count -= 1
+
+for i in range(count):
+    if i < count - 1:
+        print(2 * i + 1, end=", ")
+    else:
+        print(2 * i + 1)
